e64a55bd9e4802d1f43ccbaf03c57588f7209cf6
Aryan Khurana <aryankhurana1511@gmail.com>
2022-05-30 23:25:19 -0400

    Corrected DIY code

--- Week_02/WS02-02-DIY/BirthDate.cpp
+++ Week_02/WS02-02-DIY/BirthDate.cpp
@@ -4,7 +4,7 @@
 Full Name  : Aryan Khurana
 Student ID#: 145282216
 Email      : akhurana22@myseneca.ca
-Date of completion    : 26 May 2022
+Date of completion    : 30 May 2022
 
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 *****************************************************************************
@@ -30,6 +30,10 @@ namespace sdds {
     int noOfRecords() {
         int noOfRecs = 0;
         char ch;
+
+        // Setting pointer to the start of the file
+        rewind(fp);
+
         while (fscanf(fp, "%c", &ch) == 1) {
             noOfRecs += (ch == '\n');
         }
@@ -84,6 +88,7 @@ namespace sdds {
 
     // Reads the birth records and matches the month
     bool readBirthDate(int month) {
+        monthMatches = 0;
         bool result;
         noOfEmployees = noOfRecords();
 
--- Week_02/WS02-02-DIY/BirthDate.h
+++ Week_02/WS02-02-DIY/BirthDate.h
@@ -4,7 +4,7 @@
 Full Name  : Aryan Khurana
 Student ID#: 145282216
 Email      : akhurana22@myseneca.ca
-Date of completion    : 26 May 2022
+Date of completion    : 30 May 2022
 
 I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 *****************************************************************************
